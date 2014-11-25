f24e8299c28f5a6bc609e8cb23b95fefccb22c3a
Miguel Castillo <miguelangelcastilloalcantara@gmail.com>
2014-11-25 13:40:43 -0600

    Version 1 sin arreglos.

--- application.py
+++ application.py
@@ -1,3 +1,49 @@
-# Aqui escribe tu codigo
+#-*-coding: utf-8-*-
+import random
 
-print "Hello World"
+intentos = 0
+error=0
+x = random.randint (1, 51)
+nombre = raw_input("Ingrese su nombre:")
+print ""
+print "¡Hola!" + nombre +" Bienvenido al Juego Adivina el Número" 
+print ""
+print "INSTRUCCIONES:"
+print ""
+print """En Este Juego debes adivinar un número del 1 al 50, tendrás únicamente 
+8 intentos para encontrar el número"""
+print ""
+while intentos < 8:
+    intentos = intentos + 1
+    print "Elige un número"
+    numero = raw_input()
+
+
+    try: 
+        numero = int(numero)
+        error=1
+        
+    except:
+
+        print "solo puedes ingresar números"
+        error=0
+
+       
+ 
+        intentos = intentos + 1 
+    if numero >50 or numero <1:
+        print "Número incorrecto"
+
+    if error == 1 or error > 1:
+        if numero < x:
+            print "Tu número es mas bajo"
+        if numero > x:
+            print "Tu número es mas alto"
+        if numero == x:
+            break
+
+if numero == x:
+    print "¡FELICIDADES! ¡ADIVINASTE, HAZ GANADO!"
+        
+if numero != x:
+    print "¡LO SIENTO, EL JUEGO HA TERMINADO!"
